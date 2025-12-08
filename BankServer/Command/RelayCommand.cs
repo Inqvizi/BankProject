963b4b0c07b9e9869bda5f508f694378f00d9616
admin <admin@DESKTOP-697FU78>
2025-12-08 19:20:13 +0200

    Feature: complete Task [C0]

---
+++ BankServer/Command/RelayCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankServer.Command
+{
+    // A simple implementation of the RelayCommand pattern for command binding in MVVM.
+    internal class RelayCommand
+    {
+        // Placeholder for command implementation
+    }
+}
---
+++ BankServer/ViewModel/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BankServer.ViewModels
+{
+    // ViewModelBase implements INotifyPropertyChanged to support data binding in MVVM architecture.
+    // It provides OnPropertyChanged and SetProperty methods to simplify property change notifications.
+    // It's abstract, so it serves as a base class for other ViewModels in the application.
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T backingField, T newValue,
+            [CallerMemberName] string? propertyName = null)
+        {
+            if (Equals(backingField, newValue))
+                return false;
+
+            backingField = newValue;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
