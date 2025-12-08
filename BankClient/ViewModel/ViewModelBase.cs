9dbc71810c65770fa6b0f4f9ff7aea4fbed34d07
admin <admin@DESKTOP-697FU78>
2025-12-08 19:23:38 +0200

    Fix: add mising directories for Task [C0]

---
+++ BankClient/ViewModel/ViewModelBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankClient.ViewModel
+{
+    internal class ViewModelBase
+    {
+    }
+}
---
+++ BankServer/Model/Model.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankServer.Model
+{
+    internal class Model
+    {
+    }
+}
