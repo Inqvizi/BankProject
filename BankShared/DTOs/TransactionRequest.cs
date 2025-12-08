c6864a6df600e905c5d0bccd08d3836e6903df53
Inqvizi <Firko06y@gmail.com>
2025-12-08 20:41:58 +0200

    Implemented Shared Library: Models, Constants, DTOs

---
+++ BankShared/DTOs/TransactionRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BankShared.Enums;
+
+namespace BankShared.DTOs
+{
+    public class TransactionRequest
+    {
+        public TransactionType Type { get; set; }
+        public string AccountNumber { get; set; }
+        public decimal Amount { get; set; }
+
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+}
---
+++ BankShared/Enums/TransactionType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankShared.Enums
+{
+    public enum TransactionType
+    {
+        Deposit,
+        Withdraw,   
+    }
+}
