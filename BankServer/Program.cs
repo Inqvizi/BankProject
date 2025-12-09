f11b3c4d17062731c43d9f8bad663cfe003c7b78
Inqvizi <Firko06y@gmail.com>
2025-12-09 20:59:39 +0200

    Refactor: Convert BankServer from WPF to Console Application

---
+++ BankServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BankServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bank Server Started...");
+
+            Console.WriteLine("Server is running. Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
